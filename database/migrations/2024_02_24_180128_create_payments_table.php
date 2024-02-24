7b8531b6deb27d3f8158b357431a4604c0f7205f
projecthanif <iamustapha213@gmail.com>
2024-02-24 19:18:19 +0100

    start of a filament project

---
+++ database/migrations/2024_02_24_180128_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_id')->constrained('users', 'id');
+            $table->string('due_id')->constrained('dues', 'id');
+            $table->string('payment_amount');
+            $table->string('transaction_id')->nullable();
+            $table->timestamp('payment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
